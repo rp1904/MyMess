ddd2aed365367d7e35b9b04be36763306a3f3cdf
Akshay <roshan.patil@mobisoftinfotech.com>
2017-09-14 16:27:13 +0530

    encryption method changed

--- src/main/java/com/byb/bhojan/util/EncryptionUtils.java
+++ src/main/java/com/byb/bhojan/util/EncryptionUtils.java
@@ -1,67 +1,75 @@
 package com.byb.bhojan.util;
 
-import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.Arrays;
-import java.util.Base64;
 import java.util.Date;
 
+import org.apache.commons.codec.binary.Base64;
+
 import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
 public class EncryptionUtils {
 
-	private static SecretKeySpec secretKey;
-	private static byte[] key;
+	private static final String characterEncoding = "UTF-8";
+	private static final String cipherTransformation = "AES/CBC/PKCS5PADDING";
+	private static final String aesEncryptionAlgorithem = "AES";
 
-	public static void setKey(String myKey) {
-		MessageDigest sha = null;
+	public static String encrypt(String plainText) {
+		String encryptedText = "";
 		try {
-			key = myKey.getBytes("UTF-8");
-			sha = MessageDigest.getInstance("SHA-1");
-			key = sha.digest(key);
-			key = Arrays.copyOf(key, 16);
-			secretKey = new SecretKeySpec(key, "AES");
+			Cipher cipher = Cipher.getInstance(cipherTransformation);
+			byte[] key = getMD5(ProjectConstant.STRING_SEPERATOR).substring(0, 16).getBytes(characterEncoding);
+			SecretKeySpec secretKey = new SecretKeySpec(key, aesEncryptionAlgorithem);
+			IvParameterSpec ivparameterspec = new IvParameterSpec(key);
+			cipher.init(Cipher.ENCRYPT_MODE, secretKey, ivparameterspec);
+			byte[] cipherText = cipher.doFinal(plainText.getBytes("UTF8"));
+			encryptedText = Base64.encodeBase64String(cipherText);
+
+		} catch (Exception E) {
+			System.err.println("Encrypt Exception : " + E.getMessage());
+		}
+		return encryptedText;
+	}
+
+	public static String decrypt(String encryptedText) {
+		String decryptedText = "";
+		try {
+			Cipher cipher = Cipher.getInstance(cipherTransformation);
+			byte[] key = getMD5(ProjectConstant.STRING_SEPERATOR).substring(0, 16).getBytes(characterEncoding);
+			SecretKeySpec secretKey = new SecretKeySpec(key, aesEncryptionAlgorithem);
+			IvParameterSpec ivparameterspec = new IvParameterSpec(key);
+			cipher.init(Cipher.DECRYPT_MODE, secretKey, ivparameterspec);
+			byte[] cipherText = Base64.decodeBase64(encryptedText.getBytes("UTF8"));
+			decryptedText = new String(cipher.doFinal(cipherText), "UTF-8");
+		} catch (Exception E) {
+			System.err.println("decrypt Exception : " + E.getMessage());
+		}
+		return decryptedText;
+	}
+
+	public static String getMD5(String input) {
+		try {
+			MessageDigest md = MessageDigest.getInstance("MD5");
+			byte[] messageDigest = md.digest(input.getBytes());
+			BigInteger number = new BigInteger(1, messageDigest);
+			String hashtext = number.toString(16);
+			// Now we need to zero pad it if you actually want the full 32
+			// chars.
+			while (hashtext.length() < 32) {
+				hashtext = "0" + hashtext;
+			}
+			return hashtext;
 		} catch (NoSuchAlgorithmException e) {
-			e.printStackTrace();
-		} catch (UnsupportedEncodingException e) {
-			e.printStackTrace();
+			throw new RuntimeException(e);
 		}
-	}
-
-	public static String encrypt(String strToEncrypt) {
-		try {
-			setKey(ProjectConstant.PROJECT_NAME);
-			Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
-			cipher.init(Cipher.ENCRYPT_MODE, secretKey);
-			return Base64.getEncoder().encodeToString(cipher.doFinal(strToEncrypt.getBytes("UTF-8")));
-		} catch (Exception e) {
-			System.out.println("Error while encrypting: " + e.toString());
-		}
-		return null;
-	}
-
-	public static String decrypt(String strToDecrypt) {
-		try {
-			setKey(ProjectConstant.PROJECT_NAME);
-			Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5PADDING");
-			cipher.init(Cipher.DECRYPT_MODE, secretKey);
-			return new String(cipher.doFinal(Base64.getDecoder().decode(strToDecrypt)));
-		} catch (Exception e) {
-			System.out.println("Error while decrypting: " + e.toString());
-		}
-		return null;
 	}
 
 	public static void main(String[] args) {
 
-		Long expiresOnMillis = Dates.getMillisAfterHours(new Date(), 24);
-
-		// String link = UUID.randomUUID() + ProjectConstant.STRING_SEPERATOR +
-		// expiresOnMillis;
-
-		String link = "2fc9cb23-7a0a-4dd9-9044-cbb27bdb7ddeRP_WORLD1505330151645";
+		String link = "1505330151645RP_WORLD2fc9cb23-7a0a-4dd9-9044-cbb27bdb7ddeRP_WORLD1505330151645";
 
 		String encryptedString = EncryptionUtils.encrypt(link);
 		String decryptedString = EncryptionUtils.decrypt(encryptedString);
