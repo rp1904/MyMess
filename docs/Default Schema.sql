921ad8de8f8bb1c60e44eae51105e5dcb5232984
rp1904 <roshanpatil1904@gmail.com>
2017-10-15 23:54:45 +0530

    apks added

--- docs/Default Schema.sql
+++ docs/Default Schema.sql
@@ -4,7 +4,7 @@
 
 INSERT INTO `messdemo`.`app_versions` (`id`,`app_link`,`app_type`,`created_at`,`created_by`,`updated_at`,`updated_by`,`device_type`,`is_mandatory`,`release_date`,`release_note`,`version`) VALUES 
  (1,'http://www.mybhojan.com/resources/apk/demo/Bhojan-Mess.apk','MESS','2017-01-01 00:00:00','1','2017-01-01 00:00:00','1','ANDROID','','2017-01-01 00:00:00','Mess Base App','0.0.1'),
- (2,'http://www.mybhojan.com/resources/apk/demo/Bhojan-Member.apk','MEMBER','2017-01-01 00:00:00','1','2017-01-01 00:00:00','1','ANDROID','','2017-01-01 00:00:00','Member Base App','0.0.1'),
+ (2,'http://www.mybhojan.com/resources/apk/demo/Bhojan.apk','MEMBER','2017-01-01 00:00:00','1','2017-01-01 00:00:00','1','ANDROID','','2017-01-01 00:00:00','Member Base App','0.0.1'),
  (3,'http://www.mybhojan.com/resources/apk/demo/Bhojan-Mess.apk','MESS','2017-08-06 21:37:41','1','2017-08-06 21:37:41','1','ANDROID','','2017-08-06 21:37:41','Bug Fixes','0.0.2');
 
 INSERT INTO `messdemo`.`user_roles` (`user_role_id`,`role`) VALUES 
@@ -18,6 +18,6 @@ INSERT INTO `messdemo`.`user_roles` (`user_role_id`,`role`) VALUES
  INSERT INTO `messdemo`.`admin_settings` (`admin_setting_id`,`free_trial_days`,`notify_before_days`,`privacy_policy`,`terms_and_conditions`,`created_at`,`created_by`,`updated_at`,`updated_by`) VALUES (1, 15, 5, '', '', '2017-01-01 00:00:00', '1', '2017-01-01 00:00:00', '1');
 
  INSERT INTO `messdemo`.`mess_payment_voucher` (`mess_payment_coupen_id`,`amount`,`created_at`,`created_by`,`updated_at`,`updated_by`,`days`,`discount`,`name`) VALUES 
- ('1',0,'2017-01-01 00:00:00','1','2017-01-01 00:00:00','1',22,0,'Free Trial');
+ ('1',0,'2017-01-01 00:00:00','1','2017-01-01 00:00:00','1',15,0,'Free Trial');
  
  UPDATE users set password='$2a$10$e5KVwe1U1q0Pj/ZU5h6fpOXdInIhQ/0gYcTsSTIqLpuoYgo0dbsju';
